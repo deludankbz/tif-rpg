47d1e5c58e96767d31439773112723aaa704196a
deludankbz <luccapessoa85@gmail.com>
2026-02-17 20:23:53 -0300

    chore: add dynamic-icons; _preCreate for item types ...
    - Add item-frames used in `.inventory-grid`

--- module/documents/item.mjs
+++ module/documents/item.mjs
@@ -1,47 +1,45 @@
 /**
- * Extend the basic Item with some very simple modifications.
- * @extends {Item}
- */
+* Extend the basic Item with some very simple modifications.
+* @extends {Item}
+*/
 export class MiniD6Item extends Item {
   /**
-   * Augment the basic Item data model with additional dynamic data.
-   */
+  * Augment the basic Item data model with additional dynamic data.
+  */
   prepareData() {
-    // As with the actor class, items are documents that can have their data
-    // preparation methods overridden (such as prepareBaseData()).
     super.prepareData();
   }
-
+  
   /**
-   * Prepare a data object which defines the data schema used by dice roll commands against this Item
-   * @override
-   */
+  * Prepare a data object which defines the data schema used by dice roll commands against this Item
+  * @override
+  */
   getRollData() {
     // Starts off by populating the roll data with a shallow copy of `this.system`
     const rollData = { ...this.system };
-
+    
     // Quit early if there's no parent actor
     if (!this.actor) return rollData;
-
+    
     // If present, add the actor's roll data
     rollData.actor = this.actor.getRollData();
-
+    
     return rollData;
   }
-
+  
   /**
-   * Handle clickable rolls.
-   * @param {Event} event   The originating click event
-   * @private
-   */
+  * Handle clickable rolls.
+  * @param {Event} event   The originating click event
+  * @private
+  */
   async roll(event) {
     const item = this;
-
+    
     // Initialize chat data.
     const speaker = ChatMessage.getSpeaker({ actor: this.actor });
     const rollMode = game.settings.get('core', 'rollMode');
     const label = `[${item.type}] ${item.name}`;
-
+    
     // If there's no roll data, send a chat message.
     if (!this.system.formula) {
       ChatMessage.create({
@@ -55,7 +53,7 @@ export class MiniD6Item extends Item {
     else {
       // Retrieve roll data.
       const rollData = this.getRollData();
-
+      
       // Invoke the roll and submit it to chat.
       const roll = new Roll(rollData.formula, rollData.actor);
       // If you need to store the value first, uncomment the next line.
@@ -66,6 +64,33 @@ export class MiniD6Item extends Item {
         flavor: label,
       });
       return roll;
+    }
+  }
+
+  /**
+  * _preCreate: What do on creatin
+  */
+  async _preCreate(data, options, user) {
+    console.warn(data, this);
+    
+    switch (data.type) {
+      case "equipment":
+        this.updateSource({
+          img: "systems/miniD6/assets/icons/item/briefcase.svg",
+        });
+      break;
+        case "effect":
+        this.updateSource({
+          img: "systems/miniD6/assets/icons/item/fluffy-flame.svg",
+        });
+      break;
+      case "weapon":
+        this.updateSource({
+          img: "systems/miniD6/assets/icons/item/broad-dagger.svg",
+        });
+      break;
+      default:
+      break;
     }
   }
 }
